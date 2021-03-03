575bef9a89135f6d76699b8bce5e1950b81dcab9
EduardRo <eduardstancu@yahoo.com>
2021-03-03 10:46:38 +0200

    modification model

--- database/migrations/2021_03_03_080511_create_invoices_table.php
+++ database/migrations/2021_03_03_080511_create_invoices_table.php
@@ -15,7 +15,24 @@ class CreateInvoicesTable extends Migration
     {
         Schema::create('invoices', function (Blueprint $table) {
             $table->id();
-            
+            $table->string('invoice_serie');
+            $table->string('invoice_number');
+            $table->string('invoice_company_name');
+            $table->string('invoice_regcom');
+            $table->string('invoice_fiscal_number');
+            $table->string('invoice_city');
+            $table->text('invoice_address');
+            $table->string('invoice_region');
+            $table->string('invoice_supplier_name');
+            $table->string('invoice_supplier_regcom');
+            $table->string('invoice_supplier_fiscal_number');
+            $table->integer('invoice_supplier_capital');
+            $table->float('invoice_amount',2);
+            $table->float('invoice_amount_vat',2);
+            $table->float('invoice_total_amount',2);
+            $table->string('invoice_supplier_city');
+            $table->text('invoice_supplier_address');
+            $table->string('invoice_supplier_region');
             $table->timestamps();
         });
     }
