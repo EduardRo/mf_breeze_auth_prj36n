2cc5352c6c1a2c7b7a94ea74a2baa7ec6905c682
EduardRo <eduardstancu@yahoo.com>
2021-03-03 11:32:47 +0200

    Invoice class creation

---
+++ app/Http/Helpers/ClsInvoice.php
@@ -0,0 +1,31 @@
+<?php 
+namespace App\Http\Helpers;
+
+class ClsInvoice {
+
+    public function createInvoice(){
+
+
+    }
+
+    public function createInvoiceBody(){
+
+    }
+
+    public function createInvoiceSerie($typeOfService){
+        
+
+
+
+    }
+
+
+
+}
+
+    
+
+
+
+
+?>
